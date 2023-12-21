ea79c8aa8245e00a22cd8ebedc6aff19d8739e63
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-12-21 11:31:49 +0900

    Saveメソッド（ProductRepositoryインターフェース）の実装完了

--- app/infrastructure/mysql/repository/product_repository.go
+++ app/infrastructure/mysql/repository/product_repository.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"go-clean-arc-sauna-shop-app/app/domain/product"
+	"go-clean-arc-sauna-shop-app/app/infrastructure/mysql/db"
+	"go-clean-arc-sauna-shop-app/app/infrastructure/mysql/db/dbgen"
 )
 
 type productRepository struct{}
@@ -12,5 +14,16 @@ func NewProductRepository() *product.ProductRepository {
 }
 
 func (r *productRepository) Save(ctx context.Context, product *product.Product) error {
-
+	query := db.GetQuery(ctx)
+	if err := query.UpsertProduct(ctx, dbgen.UpsertProductParams{
+		ID:          product.ID(),
+		OwnerID:     product.OwnerID(),
+		Name:        product.Name(),
+		Description: product.Description(),
+		Price:       product.Price(),
+		Stock:       int32(product.Stock()),
+	}); err != nil {
+		return err
+	}
+	return nil
 }
