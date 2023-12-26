ee902652c1fea95251074a7c76ed8d331a98a23e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-12-26 16:28:27 +0900

    ハンドラー実装のためにGinの環境周りを構築

--- app/presentation/products/handler.go
+++ app/presentation/products/handler.go
@@ -2,6 +2,9 @@ package products
 
 import (
 	"go-clean-arc-sauna-shop-app/app/application/product"
+	"go-clean-arc-sauna-shop-app/app/presentation/settings"
+
+	"github.com/gin-gonic/gin"
 )
 
 type handler struct {
@@ -25,4 +28,13 @@ type PostProductsParams struct {
 	Description string `json:"description"`
 	Price       int64  `json:"price"`
 	Stock       int    `json:"stock"`
+}
+
+func (h handler) PostProducts(ctx *gin.Context) {
+
+	var params PostProductsParams
+	err := ctx.ShouldBindJSON(&params)
+	if err != nil {
+		settings.ReturnBadRequest(ctx, err)
+	}
 }
